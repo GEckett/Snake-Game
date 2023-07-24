1124ff636da2819725a9ae4c0922bc9496c3f468
GEckett <grant.eckett@gmail.com>
2023-07-24 21:18:15 +0100

    MOD

--- snake.py
+++ snake.py
@@ -1,4 +1,9 @@
 from turtle import Turtle
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
 class Snake:
     """Models the Snake that moves on screen"""
 
@@ -21,16 +26,18 @@ class Snake:
             self.all_segments[seg_num].goto(new_x, new_y)
         self.all_segments[0].forward(20)
 
-
     def up(self):
-        self.head.setheading(90)
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
 
     def down(self):
-        self.head.setheading(270)
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
 
     def left(self):
-        self.head.setheading(180)
-
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
 
     def right(self):
-        self.head.setheading(0)
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
