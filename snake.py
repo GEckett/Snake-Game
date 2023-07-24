cbcb2a635402c1a567618387b221b12cf18b2e58
GEckett <grant.eckett@gmail.com>
2023-07-24 21:03:21 +0100

    Day 20 complete

---
+++ snake.py
@@ -0,0 +1,35 @@
+from turtle import Turtle
+class Snake:
+    """Models the Snake that moves on screen"""
+
+    def __init__(self):
+        """Generates the initial 3 segments of the snake"""
+        self.all_segments = []
+        for segment in range(0, 3):
+            segment_1 = Turtle(shape="square")
+            segment_1.color("white")
+            segment_1.penup()
+            segment_1.goto(x=(0 - (segment * 20)), y=0)
+            self.all_segments.append(segment_1)
+
+    def move(self):
+        """Keeps all segments of the snake moving together"""
+        for seg_num in range(len(self.all_segments) - 1, 0, -1):
+            new_x = self.all_segments[seg_num - 1].xcor()
+            new_y = self.all_segments[seg_num - 1].ycor()
+            self.all_segments[seg_num].goto(new_x, new_y)
+        self.all_segments[0].forward(20)
+
+
+    def up(self):
+        self.all_segments[0].setheading(90)
+
+    def down(self):
+        self.all_segments[0].setheading(270)
+
+    def left(self):
+        self.all_segments[0].setheading(180)
+
+
+    def right(self):
+        self.all_segments[0].setheading(0)
