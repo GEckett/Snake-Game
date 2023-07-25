9af8d9bf9f2ddc4160959e9eb7ca377a7cc6fce8
GEckett <grant.eckett@gmail.com>
2023-07-25 21:33:51 +0100

    update

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 from turtle import Screen
 from snake import Snake
 import time
+from food import Food
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -10,6 +12,8 @@ screen.title("Snake")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+score_board = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -22,5 +26,8 @@ while game_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        score_board.up_score()
 
 screen.exitonclick()
---
+++ scoreboard.py
@@ -0,0 +1,22 @@
+from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ('Arial', 20, 'normal')
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.goto(0, 270)
+        self.hideturtle()
+        self.color("white")
+        self.score = 0
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.write(f"Score: {self.score}", False, align=ALIGNMENT, font=FONT)
+
+    def up_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
